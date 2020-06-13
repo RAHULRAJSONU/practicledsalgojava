78a9f84313991b205694c85f5e2c3b6bb2b97478
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-06-13 20:30:02 +0530

    problem added for linked list

---
+++ src/main/java/io/github/rahulrajsonu/dsalgo/algo/linkedlist/DeleteNode.java
@@ -0,0 +1,11 @@
+package io.github.rahulrajsonu.dsalgo.algo.linkedlist;
+
+public class DeleteNode {
+
+  public static void main(String[] args) {
+  }
+
+  public static Node insert(Node head, int val){
+    return head;
+  }
+}
